a290abbb992f4ea40405e357f97697e06924ac0d
evhane07 <evhane.kouassi@epitech.eu>
2026-09-17 14:01:21 +0000

    trying to fix the bleme due to system and pygame

--- heart.py
+++ heart.py
@@ -1,6 +1,6 @@
 import math
-import random
-import sys
+# import random
+# import sys
 import pygame
 
 pygame.init()
@@ -8,3 +8,15 @@ WIDTH, HEIGHT = 900, 700
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Cœur Mathématique Animé (Parametric Heart)")
 clock = pygame.time.Clock()
+
+
+def heart_function(t, scale=15):
+    """Équation paramétrique classique du cœur"""
+    x = 16 * (math.sin(t) ** 3)
+    y = -(
+        13 * math.cos(t)
+        - 5 * math.cos(2 * t)
+        - 2 * math.cos(3 * t)
+        - math.cos(4 * t)
+    )
+    return x * scale, y * scale
