ef1994b040db45ab130318d6a2e2033a90cc74e6
evhane07 <evhane.kouassi@epitech.eu>
2026-09-17 13:57:04 +0000

    preset

---
+++ heart.py
@@ -0,0 +1,10 @@
+import math
+import random
+import sys
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 900, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Cœur Mathématique Animé (Parametric Heart)")
+clock = pygame.time.Clock()
